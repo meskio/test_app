07e038ac4b5df93e5c3f81fdd7d67c3832f00c0e
meskio <meskio@sindominio.net>
2019-06-08 18:07:51 +0200

    tst 3

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ console.log("Kubia server starting...");
 var handler = function(request, response) {
 	  console.log("Received request from " + request.connection.remoteAddress);
 	  response.writeHead(200);
-	  response.end("2You've hit " + os.hostname() + "\n");
+	  response.end("3You've hit " + os.hostname() + "\n");
 };
 var www = http.createServer(handler);
 www.listen(8080);
